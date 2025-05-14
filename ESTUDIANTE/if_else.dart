7bfe5fc39f920537a8e0f43dcdf6edfd1044c486
Garabatec <garabatec@gmail.com>
2025-05-13 21:09:07 -0500

    Update if_else.dart

--- ESTUDIANTE/if_else.dart
+++ ESTUDIANTE/if_else.dart
@@ -1,1 +1,15 @@
+void main() {
+  int edad = 50; 
+  if (edad >= 18) {
+    print("Eres mayor de edad");
+  } else if (edad >= 14 && edad < 18) {
+    print("Eres adolescente");
+  } else if (edad > 4 && edad < 14) {
+    print("Es niño");
+  } else if (edad > 0 && edad <= 4) {
+    print("Es un bebé");
+  } else {
+    print("Edad no válida");
+  }
+}
 
